a496b6c2bee07a61b9938eedd617d11e676b95cb
mrajay001 <ajayh393798@gmail.com>
2024-12-08 09:51:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -136,8 +136,8 @@ async def start_command(client: Client, message: Message):
                     InlineKeyboardButton('🍁 ᴍᴏᴠɪᴇs', url='https://t.me/+SrRuLfKjAY0xZDU1')
                 ],
                 [
-                    InlineKeyboardButton('🔞 ʜᴇɴᴛᴀɪ  🚀', url='https://t.me/+Lue7ExXUBak1NDhl'),
-                    InlineKeyboardButton('🔞 ᴊᴀᴘᴀɴᴇꜱᴇ 🚀', url='https://t.me/+Lue7ExXUBak1NDhl')
+                    InlineKeyboardButton('🔞 ʜᴇɴᴛᴀɪ  ', url='https://t.me/+Lue7ExXUBak1NDhl'),
+                    InlineKeyboardButton('🔞 ᴊᴀᴘᴀɴᴇꜱᴇ ', url='https://t.me/+jD4qzJeB6E0zMjBl')
                 ]
                 ]
             
